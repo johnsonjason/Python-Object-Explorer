9ef0fd4fcad5acd1e1976889406cd6a452dfd67d
Jason Johnson <jasonmjohnson1@gmail.com>
2021-05-22 22:10:16 -0400

    Delete FrameHandler.h

--- cpp-src/FrameHandler.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "pch.h"
-
-__declspec(dllexport) PyFrameObject* GetFrame(PyThreadState* tstate, PyCodeObject* code, PyObject* globals, PyObject* locals);
-
-void HandleFunction();
