2b4b6156c1dba207c05f12e994ac77ffb32f224e
Jason Johnson <jasonmjohnson1@gmail.com>
2021-05-23 21:36:37 -0400

    Update ObjEnum.cpp

--- cpp-src/ObjEnum.cpp
+++ cpp-src/ObjEnum.cpp
@@ -13,7 +13,7 @@ PySet EnumerateObject(std::string ObjectPath, std::string BaseName, bool UseGlob
             PathTokens.push_back(Path);
         }
     }
-
+    
     PySet Module;
     if (UseGlobals) {
         Module = FindGlobal(BaseName);
@@ -21,6 +21,7 @@ PySet EnumerateObject(std::string ObjectPath, std::string BaseName, bool UseGlob
     else {
         Module = FindModule(BaseName);
     }
+
 
     PyObject* ReturnObject = std::get<0>(Module);
     PyObject* ReturnObjectList = std::get<1>(Module);
@@ -93,4 +94,100 @@ PyObject* EnumerateObjectReal(std::string ObjectPath, std::string BaseName, bool
 //
 PyObject* EnumerateObjectOutput(std::string ObjectPath, std::string BaseName, bool UseGlobals) {
     return std::get<1>(EnumerateObject(ObjectPath, BaseName, UseGlobals));
+}
+
+//
+// Build a tree for an object based on the following parameters:
+// ObjectPath - The path to an object after its BaseName (acting as a root)
+// BaseName - The root of an object's path, it is separate from the path because root names can contain periods which interferes with parsing
+// UseGlobals - whether we search in interp->modules or _frame->f_globals
+// LastObject - the last object of the path, can prevent specific crashes of attributeless objects
+//
+void EnumerateObjectTree(std::string ObjectPath, std::string BaseName, bool UseGlobals, std::string LastObject) {
+    std::istringstream ObjectDelimiter(ObjectPath);
+    std::vector<std::string> PathTokens;
+    std::string Path;
+
+    while (std::getline(ObjectDelimiter, Path, '.')) {
+        if (!Path.empty()) {
+            PathTokens.push_back(Path);
+        }
+    }
+
+    PySet Module;
+
+    if (UseGlobals) {
+        //
+        // Get the base object set within the globals
+        //
+        Module = FindGlobal(BaseName);
+    }
+    else {
+        //
+        // Get the base object set within the modules
+        //
+        Module = FindModule(BaseName);
+    }
+
+    PyObject* ReturnObject = std::get<0>(Module);
+    PyObject* ReturnObjectList = std::get<1>(Module);
+
+    if (ReturnObject == nullptr) {
+        return;
+    }
+
+    //
+    // Return a tree of base object names
+    //
+    if (LastObject == "_NONE_" || ObjectPath == "_NONE_") {
+        std::string SocketQuery;
+        for (Py_ssize_t v = 0; v < Py_SIZE(ReturnObjectList); v++) {
+            PyObject* ListItem = PyList_GetItem(ReturnObjectList, v);
+            SocketQuery += std::string(PyUnicode_AsUTF8(ListItem)) + " (" + Py_TYPE(Py_TYPE(std::get<0>(Module))->tp_getattro(ReturnObject, ListItem))->tp_name + ") [" + int_to_hex((DWORD_PTR)PyObject_GetAttr(ReturnObject, ListItem)) + "]|";
+        }
+        send(GlobalReceiver, SocketQuery.c_str(), SocketQuery.size(), 0);
+        return;
+    }
+
+    for (size_t i = 0; i < PathTokens.size(); i++) {
+        PyObject* ObjectName = GetListItem(ReturnObjectList, Py_SIZE(ReturnObjectList), PathTokens[i]);
+
+        if (ObjectName == nullptr) {
+            return;
+        }
+
+        ReturnObject = Py_TYPE(std::get<0>(Module))->tp_getattro(ReturnObject, ObjectName);
+
+        if (ReturnObject == nullptr) {
+            return;
+        }
+
+        ReturnObjectList = GetObjDir(ReturnObject);
+
+        if (PyUnicode_AsUTF8(ObjectName) == LastObject && i == PathTokens.size() - 1) {
+            std::string SocketQuery;
+            bool Abstract = false;
+            for (Py_ssize_t v = 0; v < Py_SIZE(ReturnObjectList); v++) {
+                PyObject* ListItem = PyList_GetItem(ReturnObjectList, v);
+                if (std::string(PyUnicode_AsUTF8(ListItem)) == "__abstractmethods__") {
+                    Abstract = true;
+                }
+
+                if (Abstract == false) {
+
+                    if (PyObject_HasAttr(ReturnObject, ListItem) != 0) {
+                        PyObject* ListObject = PyObject_GetAttr(ReturnObject, ListItem);
+                        SocketQuery += std::string(PyUnicode_AsUTF8(ListItem)) + " (" + std::string(Py_TYPE(ListObject)->tp_name) + ") [" + int_to_hex((DWORD_PTR)ListObject) + "]|";
+                    }
+                }
+                else {
+                    SocketQuery += std::string(PyUnicode_AsUTF8(ListItem)) + " (" + "Unknown" + ")|";
+                }
+            }
+            Abstract = false;
+            send(GlobalReceiver, SocketQuery.c_str(), SocketQuery.size(), 0);
+            return;
+        }
+
+    }
 }
